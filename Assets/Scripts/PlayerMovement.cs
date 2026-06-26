39e9d117d8491195aa2a71ad175790e8618870ea
Renanmc132 <renanmc100@gmail.com>
2026-06-26 11:47:55 -0300

    feat : Adiciona movimentação do player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,58 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private Rigidbody2D _rb;
+    private Animator _anim;
+
+    private float jumpHeight = 10f;
+    private float rotationSpeed = -90f;
+    private float jumpRotation = 48f;
+
+    private float gravity = 9.8f;
+    private float gravityAcceleration = 2f;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
     }
+
+    private void FixedUpdate()
+    {
+        Gravity();
+        transform.localRotation *= Quaternion.Euler(transform.rotation.x, transform.rotation.y, rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    
+
+    private void Jump()
+    {
+        transform.localRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, jumpRotation);
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpHeight);
+    }
+
+    private void Gravity()
+    { 
+        float gravityForce = gravity * gravityAcceleration * Time.fixedDeltaTime;
+
+
+        _rb.linearVelocity -= new Vector2(0, gravityForce);
+
+    }
+
+
 }
